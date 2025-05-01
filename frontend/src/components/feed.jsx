87a91eb405ad3a8a057b28125f8f5fdd4055a943
Nitin Rajput <nitinrajput0093@gmail.com>
2025-05-01 15:57:54 +0530

    some deployment fixes

--- frontend/src/components/feed.jsx
+++ frontend/src/components/feed.jsx
@@ -11,7 +11,7 @@ function Feed() {
     const[showPostform,setshowPostform]=useState(false)
 
     const fetchPosts = async () => {
-        const res = await fetch(`${process.env.REACT_APP_API_URL}/api/post/getposts`, {
+        const res = await fetch(`https://chat-application-wqtj.onrender.com/api/post/getposts`, {
             method: 'GET',
             
         })
